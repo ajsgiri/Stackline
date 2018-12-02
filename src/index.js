f18508e83898fa34a59cf4471b49ca58f374cae8
Ajay Giri <asgiri@ucsd.edu>
2018-12-02 12:13:41 -0800

    setup react and parcel

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from "react-dom"
+
+function App (props) {
+    return (<p>hi</p>)
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
